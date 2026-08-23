1aa5826162e888613ecb285cf581b5c077c0c604
Mo'men Tamer <moamenmosa288@gmail.com>
2026-08-23 20:23:30 +0300

    task3: completed

--- day1/tasks.js
+++ day1/tasks.js
@@ -3,3 +3,12 @@ console.log('JS is running successfully');
 
 // Task 2
 window.alert('Ready to practice!');
+
+// Task 3
+var productName = "laptop";
+var price = 15000;
+var isAvailable = true;
+
+console.log(`product name: ${productName} | its type: ${typeof productName}`);
+console.log(`price: ${price} | its type: ${typeof price}`);
+console.log(`is available: ${isAvailable} | its type: ${typeof isAvailable}`);
