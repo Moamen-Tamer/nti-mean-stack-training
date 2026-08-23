c39e281bd3fe6470add26d6ba37135ba10cc5dec
Mo'men Tamer <moamenmosa288@gmail.com>
2026-08-23 20:28:50 +0300

    task4: completed

--- day1/tasks.js
+++ day1/tasks.js
@@ -12,3 +12,6 @@ var isAvailable = true;
 console.log(`product name: ${productName} | its type: ${typeof productName}`);
 console.log(`price: ${price} | its type: ${typeof price}`);
 console.log(`is available: ${isAvailable} | its type: ${typeof isAvailable}`);
+
+// Task 4
+document.getElementById('mainTitle').innerText = "Practice Time";
