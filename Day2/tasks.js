a925477c308a7690227ea4558b5b264a72bc9bdf
Mo'men Tamer <moamenmosa288@gmail.com>
2026-08-24 19:43:08 +0300

    task03: completed

--- Day2/tasks.js
+++ Day2/tasks.js
@@ -7,3 +7,12 @@ for(var i = 1; i <= 10; i++) {
 for(var i = 2; i <= 20; i += 2) {
     console.log(`Available seats: ${i}`);
 }
+
+// Task 3
+var totalSales = 0;
+
+for(var i = 1; i <= 15; i++) {
+    totalSales += i;
+}
+
+console.log(`Total Sales: ${totalSales}`);
